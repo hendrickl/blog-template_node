b6dcae0df8bc214c247a70e5c927f21fe530db0f
Hendrick L <h.lincertin@gmail.com>
2021-11-12 18:16:38 +0100

    feat: middleware : add public folder

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require("express");
+const morgan = require("morgan");
 
 // express app
 const app = express();
 
-// listen for requests
-app.listen(3000);
-
 // register view engine
 app.set("view engine", "ejs");
 // app.set('views', 'myviews');
+
+// listen for requests
+app.listen(3000);
+
+// Middleware & static files : wich we want to make public
+app.use(express.static("public"));
+
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   const blogs = [
